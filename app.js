8e618c3909fe8500660908ef4ae2d3ef1f642cce
Cumbie <bcumbie@una.edu>
2024-01-25 16:44:00 -0600

    turned over puzzle pieces for nov

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5500;
+const port = process.env.PORT || 5000;
+// set the view engine to ejs
+let path = require('path');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', function (req, res) {
+// use res.render to load up an ejs view file
+
+let myTypeServer = "The Peacemaker";
+
+app.get('/', function(req, res) {
+
+  res.render('index', {
+   
+    myTypeClient: myTypeServer 
+
+  });
+  
+});
+
+
+app.get('/send', function (req, res) {
   
     res.send('Hello World from Express ')
 })
@@ -10,5 +29,5 @@ app.get('/', function (req, res) {
 // app.listen(3000)
 
 app.listen(port, () => {
-  console.log(`mike app listening on port ${port}`)
+  console.log(`nov app listening on port ${port}`)
 })
